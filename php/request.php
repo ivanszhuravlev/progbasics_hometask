ea6d1f53cacb668bfb1ea172894effbdfff86967
Ivan Zhuravlev <ivanszhuravlev@gmail.com>
2016-01-29 12:33:18 +0300

    Get links from a number of pages

--- php/request.php
+++ php/request.php
@@ -39,6 +39,50 @@ function condition($link) {
     return $condition;
 }
 
+/*
+** Заполняем массив ссылками с $pages_count страниц.
+*/
+function search_anchors($dom_object, $url, $pages_count) {
+    
+    $suitable_links = array();
+    $next_url = $url;
+    
+    for ($current_page = 1; $current_page <= $pages_count; $current_page++) {
+        
+        $url = $next_url;
+        
+        /*
+        ** Это функция из библиотеки Simple HTML DOM,
+        ** возвращает DOM файла.
+        */
+        $dom_object->load_file($url);
+
+        $anchors = $dom_object->find('a');
+
+        foreach ($anchors as $anchor){ 
+            if (condition($anchor->href)) {
+                if (stristr($anchor->href, "http://") === false && stristr($anchor->href, "https://") === false) {
+                    $href = $url . $anchor->href;
+                } else {
+                    $href = $anchor->href;
+                }
+
+                if (!in_array($href, $suitable_links)) {
+                    array_push($suitable_links, $href);
+                }
+            }
+            
+            if (!stristr($anchor->href, "/page/" . ($current_page + 1) . "/") === false) {
+                $next_url = $anchor->href;
+            }
+        } 
+        
+        
+    }
+    
+    return $suitable_links;
+}
+
 $url = $_POST['field'];
 
 
@@ -50,34 +94,24 @@ if (isset($url)){
     
     /*
     ** Подключаем библиотеку Simple HTML DOM,
-    ** создаем объект и подгружаем код страницы.
+    ** создаем объект.
     */
     include('./lib/simple_html_dom.php');
     
     $html = new simple_html_dom();
-    
-    $html->load_file($url);
     /*
     ** --------------------
     */
     
-    $anchors = $html->find('a');
+    $pages_count = 20;
     
-    $suitable_links = array();
+    $suitable_links = search_anchors($html, $url, $pages_count);
     
-    foreach ($anchors as $anchor){ 
-        if (condition($anchor->href)) {
-            if (stristr($anchor->href, "http://") === false && stristr($anchor->href, "https://") === false) {
-                $href = $url . $anchor->href;
-            } else {
-                $href = $anchor->href;
-            }
-            
-            if (!in_array($href, $suitable_links)) {
-                array_push($suitable_links, $href);
-            }
-        }
-    }    
+    var_dump($suitable_links);
+    
+    
+    
+    
     
     
 }
